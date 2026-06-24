ec62e7580dfe2703616a846f22d15e06d11f6077
kunalsanwlecha <kunalsanwlecha992@gmail.com>
2026-06-25 00:33:54 +0530

    Create axi_sequencer.sv

---
+++ AGENT/axi_sequencer.sv
@@ -0,0 +1,11 @@
+`ifndef AXI_SEQUENCER
+`define AXI_SEQUENCER
+`include "uvm_macros.svh"
+import uvm_pkg::*;
+class axi_sequencer extends uvm_sequencer #(axi_seq_item);
+  `uvm_component_utils(axi_sequencer)
+  function new(string name = "axi_sequencer", uvm_component parent);
+    super.new(name, parent);
+  endfunction
+endclass
+`endif
